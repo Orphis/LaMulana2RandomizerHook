ff3da5f74790d299dd7ee8a5268cf17c605990b6
Florent Castelli <florent.castelli@gmail.com>
2019-03-16 19:05:48 +0100

    Add project files.

---
+++ LaMulana2RandomizerHook/LM2RHooks/patch_MojiScript.cs
@@ -0,0 +1,45 @@
+﻿using L2Base;
+using MonoMod;
+
+#pragma warning disable CS0626 // orig_ method is marked external and has no attributes on it.
+namespace LM2RHooks
+{
+    [MonoModPatch("L2Word.MojiScript")]
+    public class patch_MojiScript
+    {
+        [MonoModIgnore]
+        private L2Word.COMMDATA[] commbuff;
+
+        private extern bool orig_getCommData(string data, int start, int num);
+        private bool getCommData(string data, int start, int num)
+        {
+            bool res = orig_getCommData(data, start, num);
+
+            if (replace_item != null)
+            {
+                commbuff[0].sbuff.Length = 0;
+                commbuff[0].sbuff.Append(replace_item);
+                replace_item = null;
+            }
+
+            return res;
+        }
+
+        private extern void orig_com_take(string data, int raw);
+        private void com_take(string data, int raw)
+        {
+            this.getCommData(data, raw, 3);
+            if (this.commbuff[0].sbuff.ToString().Equals("Xelputter"))
+            {
+                replace_item = "La-Mulana";
+            } else
+            {
+                replace_item = null;
+            }
+
+            orig_com_take(data, raw);
+        }
+
+        private string replace_item;
+    }
+}
